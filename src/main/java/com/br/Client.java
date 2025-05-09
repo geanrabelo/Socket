31a66e8123b6805d042083a503ba88e7c647d75f
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-09 09:49:19 -0300

    Client e Server criado com as configurações básicas

---
+++ src/main/java/com/br/Client.java
@@ -0,0 +1,11 @@
+package com.br;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 4000);
+
+    }
+}
---
+++ src/main/java/com/br/Server.java
@@ -0,0 +1,13 @@
+package com.br;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(4000);
+        Socket socket = serverSocket.accept();
+        System.out.println("Client connected!");
+    }
+}
